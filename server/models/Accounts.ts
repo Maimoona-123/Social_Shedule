2c33e0f3c9613c283f2a2aa89d530da9fad4e1b3
Maimoona <maimonaaltaf7@gmail.com>
2026-06-30 23:25:28 +0500

    created API for social accounts creations

---
+++ server/models/Accounts.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'; 
+
+const accountSchema = new mongoose.Schema({
+    user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+    platform: {type: String, enum: ["Twitter", "LinkedIn", "facebook", "instagram", "facebook_page",
+        "linkedin_page", "instagram_buisness"], required: true},
+
+        handle: {type: String, required: true},
+        zernioAccountId: {type: String},
+        accessToken: {type: String},
+        refreshToken: {type: String},
+        tokenExpiresAt: {type: Date},
+        status: {type: String, enum: ["connected", "disconnect"], default: "connected"},
+        avatatUrl: {type: String}
+
+}, {timestamps: true});
+
+export const Account = mongoose.model("Account", accountSchema)
--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import socialAuthRouter from "./routes/socialAuthRoutes.js";
 
 const app = express();
 
@@ -19,6 +21,8 @@ app.get('/', (_req: Request, res: Response) => {
     res.send('Server is Live!');
 });
 
+app.use('/api/auth', authRouter)
+app.use('api/oauth', socialAuthRouter)
 
 // GLobar error handler
 app.use((err: any, _req: Request, res: Response, _next: NextFunction) =>{
